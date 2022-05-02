0c6129b089a56fb620526964390c814a1b222d21
Luis Ojeda <luisma0330@hotmail.com>
2022-05-02 12:55:16 -0500

    Agregando la prueba con la funcionalidad que se desea añadir

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,11 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+describe("Unit test for Spider-man class", () => {
+    test('1) Create an Spider-man object', () => {
+      const andrewGarfield = new Spiderman("The Amazing Spiderman", 31, "Andrew Garfield", 2, "Columbia Pictures")
+
+      expect(andrewGarfield.name).toBe("The Amazing Spiderman")
+      expect(andrewGarfield.age).toBe(31)
+      expect(andrewGarfield.actor).toBe("Andrew Garfield")
+      expect(andrewGarfield.movies).toBe(2)
+      expect(andrewGarfield.studio).toBe("Columbia Pictures")
     });
   })
